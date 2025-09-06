712ce67b8405261c2b45abf7572ae69b296757c9
bman3663 <bdspin17@gmail.com>
2025-09-06 23:23:43 +0300

    In this project, I built a currency converter app using React. I used hooks like useState and useEffect to manage state and side effects, created reusable components such as InputBox, and handled user interactions for swapping currencies and converting values. I implemented controlled components for form inputs, passed props to customize component behavior, and styled the interface with Tailwind CSS. This project showcases my use of React features including component composition, hooks, event handling, and conditional rendering.

--- 06currencyconverter/src/App.jsx
+++ 06currencyconverter/src/App.jsx
@@ -2,32 +2,78 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
+import { InputBox } from './components/index.js'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState('eur')
+  const [to, setTo] = useState('usd')
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+  const options = Object.keys(currencyInfo)
+  // console.log("Options: " + options)
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to])
+  }
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setAmount(convertedAmount)
+    setConvertedAmount(amount)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className=' w-full h-screen flex flex-wrap 
+      justify-center items-center bg-cover bg-no-repeat'
+      style={{backgroundImage: `url(https://images.pexels.com/photos/6266453/pexels-photo-6266453.jpeg)`}}>
+        <div className='w-full max-w-md mx-auto border border-gray-50
+        rounded-lg p-5 backdrop-blur-sm bg-white/30'>
+          <form onSubmit={(e) =>{
+            e.preventDefault()
+            convert()
+            }} >
+            <div className='w-full mb-8'>
+              <InputBox
+                label="from"
+                amount={amount}
+                onAmountChange={(amount) => setAmount(amount)}  
+                onCurrencyChange={(currency) => setFrom(currency)} 
+                currencyOptions={options}
+                selectedCurrency={from}
+              />
+            </div>
+            <div className="relative w-full h-0.5">
+              <button className='absolute left-1/2 
+              -translate-x-1/2 -translate-y-1/2 border-2 
+              border-white rounded-md bg-blue-600 
+              text-white px-2 py-0.5' 
+              onClick={swap}>Swap</button>
+            </div>
+            <div className='w-full mt-8 mb-2'>
+              <InputBox
+                label="to"
+                amount={convertedAmount}
+                amountDisabled={true}
+                onAmountChange={(amount) => setConvertedAmount(amount)}  
+                onCurrencyChange={(currency) => setTo(currency)} 
+                currencyOptions={options}
+                selectedCurrency={to}
+              />
+            </div>
+            <div>
+            <button className='w-full bg-blue-600 text-white px-4 py-3 rounded-lg' 
+              type='submit'>Convert {from.toUpperCase()} to {to.toUpperCase()}</button>
+            </div>
+          </form>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
